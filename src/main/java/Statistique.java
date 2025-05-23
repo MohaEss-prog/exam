4586b95e75ff8224ab303df2a6fa838824557dfa
ESSAOUDI Mohammed <12012244@q10429.iutv.univ-paris13.fr>
2025-05-23 12:07:43 +0200

    refactore projet

--- src/main/java/Statistique.java
+++
@@ -1,7 +0,0 @@
-public class Statistique {
-    Calculatrice calculatrice = new Calculatrice();
-
-    public float moyenne(float x, float y) {
-        return calculatrice.div(calculatrice.add(x, y), 2);
-    }
-}
