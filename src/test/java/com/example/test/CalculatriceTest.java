64e178ffeab83dab081b052199f9e89d0135939e
MohaEss-prog <sd01.med@gmail.com>
2025-05-23 12:28:53 +0200

    Add files via upload

---
+++ src/test/java/com/example/test/CalculatriceTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assertions.*;
+import org.junit.jupiter.Test;
+
+public class CalculatriceTest {
+
+    Calculatrice calc = new Calculatrice();
+
+    @Test
+    public void testAdd() {
+        Assertions.assertEquals(5.0f, calc.add(2.0f, 3.0f));
+    }
+
+    @Test
+    public void testDiv() {
+        Assertions.assertEquals(2.0f, calc.div(6.0f, 3.0f));
+    }
+
+    @Test
+    public void testDivByZero() {
+        Assertions.assertThrows(ArithmeticException.class, () -> calc.div(5.0f, 0));
+    }
+
+    @Test
+    public void testMult() {
+        Assertions.assertEquals(6.0f, calc.mult(2.0f, 3.0f));
+    }
+
+    @Test
+    public void testMinus() {
+        Assertions.assertEquals(1.0f, calc.minus(4.0f, 3.0f));
+    }
+
+    @Test
+    public void testMinusNegative() {
+        Assertions.assertEquals(-2.0f, calc.minus(1.0f, 3.0f));
+    }
+}
